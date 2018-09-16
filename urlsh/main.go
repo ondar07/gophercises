9ac0b469a077152d0ad34fc549e77f30ac4f14ce
Adygzhy Ondar <ondar07@phystech.edu>
2018-09-16 15:10:03 +0300

    urlshortener

---
+++ urlsh/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"gophercises/urlsh/urlshort"
+)
+
+func main() {
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+	//fmt.Println("Starting the server on :9090")
+	//http.ListenAndServe(":9090", yamlHandler)
+
+	// Build the YAMLHandler using the mapHandler as the
+	// fallback
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
+	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :9090")
+	http.ListenAndServe(":9090", yamlHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
